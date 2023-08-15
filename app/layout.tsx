95b3aff830bd8deba72bae9d60744f42b4d5fcfb
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-08-15 05:35:16 -0700

    Implemented the sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,6 @@ export default function RootLayout({
           <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
              {children}
           </ThemeProvider>
-         
           </body>
       </html>
     </ClerkProvider>
